24e3e4d4b65dfa516e3ddc4b0d22d5a61b213c03
Nguyen Nghia Nhan <nghianhan30092k5@gmail.com>
2026-08-18 14:44:25 +0700

    MiniProject - X

--- views/client/layouts/footer.php
+++ views/client/layouts/footer.php
@@ -3,7 +3,7 @@
         <div class="row">
             <div class="col-md-4 mb-4">
                 <h5 class="text-warning mb-3"><i class="bi bi-shop me-2"></i>MiniShop</h5>
-                <p class="text-muted">Cửa hàng chuyên cung cấp các sản phẩm công nghệ, mỹ phẩm và phụ kiện máy tính chính hãng với giá cả hợp lý nhất.</p>
+                <p class="text-white-50">Cửa hàng chuyên cung cấp các sản phẩm nước hoa chính hãng với giá cả hợp lý nhất.</p>
                 <div class="d-flex gap-3 mt-3">
                     <a href="#" class="text-light fs-4"><i class="bi bi-facebook"></i></a>
                     <a href="#" class="text-light fs-4"><i class="bi bi-instagram"></i></a>
@@ -14,16 +14,16 @@
             <div class="col-md-4 mb-4">
                 <h5 class="mb-3">Thông tin</h5>
                 <ul class="list-unstyled">
-                    <li class="mb-2"><a href="#" class="text-muted text-decoration-none hover-white">Giới thiệu</a></li>
-                    <li class="mb-2"><a href="#" class="text-muted text-decoration-none hover-white">Chính sách bảo mật</a></li>
-                    <li class="mb-2"><a href="#" class="text-muted text-decoration-none hover-white">Chính sách đổi trả</a></li>
-                    <li class="mb-2"><a href="#" class="text-muted text-decoration-none hover-white">Điều khoản dịch vụ</a></li>
+                    <li class="mb-2"><a href="#" class="text-white-50 text-decoration-none hover-white">Giới thiệu</a></li>
+                    <li class="mb-2"><a href="#" class="text-white-50 text-decoration-none hover-white">Chính sách bảo mật</a></li>
+                    <li class="mb-2"><a href="#" class="text-white-50 text-decoration-none hover-white">Chính sách đổi trả</a></li>
+                    <li class="mb-2"><a href="#" class="text-white-50 text-decoration-none hover-white">Điều khoản dịch vụ</a></li>
                 </ul>
             </div>
             
             <div class="col-md-4 mb-4">
                 <h5 class="mb-3">Liên hệ</h5>
-                <ul class="list-unstyled text-muted">
+                <ul class="list-unstyled text-white-50">
                     <li class="mb-2"><i class="bi bi-geo-alt-fill me-2 text-warning"></i> Địa chỉ: 123 Đường ABC, TP. HCM</li>
                     <li class="mb-2"><i class="bi bi-telephone-fill me-2 text-warning"></i> Điện thoại: 0123 456 789</li>
                     <li class="mb-2"><i class="bi bi-envelope-fill me-2 text-warning"></i> Email: contact@minishop.com</li>
@@ -33,7 +33,7 @@
         
         <hr class="border-secondary my-4">
         
-        <div class="text-center text-muted">
+        <div class="text-center text-white-50">
             <p class="mb-0">&copy; 2026 MiniShop. All rights reserved.</p>
         </div>
     </div>
